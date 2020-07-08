4ff47c0b82172394a8ced54043d02acc6b774508
Henry <henrysoverholt@gmail.com>
2020-07-08 12:23:54 -0400

    routing for site and new component for first project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { NumberOneComponent } from "./number-one/number-one.component";
+const appRoutes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "001", component: NumberOneComponent },
+  { path: "**", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+];
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, NumberOneComponent],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/number-one/number-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-one',
+  templateUrl: './number-one.component.html',
+  styleUrls: ['./number-one.component.css']
+})
+export class NumberOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
