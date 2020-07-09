cd50b0cc33c460d219bd132875a295e7085f24ac
Henry <henrysoverholt@gmail.com>
2020-07-08 20:27:51 -0400

    added component for challenge two

---
+++ src/app/number-two/number-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-two',
+  templateUrl: './number-two.component.html',
+  styleUrls: ['./number-two.component.css']
+})
+export class NumberTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
